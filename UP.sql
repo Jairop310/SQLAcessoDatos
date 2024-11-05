0e4d3a83026fb65176059bd1d3b7223d48478dd1
unknown <jorgeten2@gmail.com>
2024-11-05 12:17:45 +0100

    Inserccion campos valoracion

--- UP.sql
+++ UP.sql
@@ -1,24 +1,27 @@
 CREATE TABLE videojuegos (    
     name VARCHAR(100) Primary Key,
     year INT,
-    genero VARCHAR(50)
+    genero VARCHAR(50),
+    valoracion INT,
+    valoracion_total INT
 );
 CREATE TABLE personajes (
     name VARCHAR(100) Primary Key,
     tipo VARCHAR(50),
     videojuego_name varchar(100),
+    valoracion INT,
     FOREIGN KEY (videojuego_name) REFERENCES videojuegos(name)
 );
 
-INSERT INTO videojuegos (name, year, genero) VALUES 
-('Fornite', 2017, 'Accion'),
-('League of Legends', 2009, 'MOBA'),
-('Valorant', 2020, 'FPS'),
-('Among Us', 2018, 'Social Deduction');
+INSERT INTO videojuegos (name, year, genero, valoracion) VALUES 
+('Fornite', 2017, 'Accion', 80),
+('League of Legends', 2009, 'MOBA', 70),
+('Valorant', 2020, 'FPS', 70),
+('Among Us', 2018, 'Social Deduction', 70);
 
-INSERT INTO personajes (name, tipo, videojuego_name) VALUES 
-('John Wick', 'Arma', 'Fornite'),         
-('Zed', 'Ninja', 'League of Legends'),             
-('Fizz', 'Pez', 'League of Legends'),               
-('Pepe', 'Tanque', 'Valorant'),           
-('Impostor', 'Infiltrado', 'Among Us');    
+INSERT INTO personajes (name, tipo, videojuego_name, valoracion) VALUES 
+('John Wick', 'Arma', 'Fornite', 60),         
+('Zed', 'Ninja', 'League of Legends', 40),             
+('Fizz', 'Pez', 'League of Legends', 50),               
+('Pepe', 'Tanque', 'Valorant', 70),           
+('Impostor', 'Infiltrado', 'Among Us', 20);    
